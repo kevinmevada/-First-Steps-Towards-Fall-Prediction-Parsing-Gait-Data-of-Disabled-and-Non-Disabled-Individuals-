73a6ec3e3d7d5bdac6a5744d88cb95db4f6b4253
Kevin Mevada <mevadakevin@gmail.com>
2026-01-29 02:40:07 -0600

    Setup.py Completed

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages, setup
+from typing import List
+
+
+def get_requirements() -> List[str]:
+    try:
+        with open("requirements.txt") as f:
+            return [line.strip() for line in f if line.strip() and line.strip() != "-e ."]
+    except FileNotFoundError:
+        return []
+
+
+setup(
+    name="FIRST STEPS TOWARDS FALL PREDICTION",
+    version="0.0.1",
+    author="Kevin Mevada",
+    author_email="kmevada@hawk.illinoistech.edu",
+    packages=find_packages(),
+    install_requires=get_requirements(),
+)
